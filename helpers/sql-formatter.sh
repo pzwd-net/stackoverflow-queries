4d13a92605fd119aea4e42885daf758a604bde74
pipecraft <pipecraft.net@gmail.com>
2021-09-15 17:34:00 +0800

    Add newlines between queries

--- helpers/sql-formatter.sh
+++ helpers/sql-formatter.sh
@@ -2,4 +2,4 @@
 
 cd $(dirname $0)
 
-find ../queries -type f -name *.sql -exec sh -c 'cat "$@" | npx sql-formatter -u -o "$@"' sh {} \;
+find ../queries -type f -name *.sql -exec sh -c 'cat "$@" | npx sql-formatter -u --lines-between-queries 2 -o "$@"' sh {} \;
